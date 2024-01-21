084adc95b8268f950b59dd5b95d0ac17311ad592
Tashila Fernando <tashila.dev@gmail.com>
2024-01-21 11:59:55 +0530

    task: added in router and morgan middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+
+import router from "./router";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res, next) => {
   setTimeout(() => {
     next(new Error("hello"));
   }, 1);
 });
+
+app.use("/api", router);
 
 export default app;
