ff2e4483e55237f5ced6d80ef7d345d12e7d57bd
Tashila Fernando <tashila.dev@gmail.com>
2024-01-21 12:04:44 +0530

    task: added in cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import router from "./router";
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
   setTimeout(() => {
@@ -14,5 +18,10 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/api", router);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ message: `had an error: ${err.message}` });
+});
 
 export default app;
